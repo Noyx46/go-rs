4ee611a7b49d8ecf9131b2cfc981aeec9611393d
Noyx46 <Noyx46@users.noreply.github.com>
2022-04-16 03:24:20 +0000

    deploy: e4795da884d845c5ef4b574eb48e8d2b541cb20a

--- index-b02a9cec97d53753.js
+++ index-b02a9cec97d53753.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-b21cb40e13f1bc93_bg.wasm', import.meta.url);
+        input = new URL('index-b02a9cec97d53753_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
