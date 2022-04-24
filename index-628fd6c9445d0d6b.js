fc655e709524f6e781ad3a0828277ed6cddf58c2
Noyx46 <Noyx46@users.noreply.github.com>
2022-04-24 23:40:01 +0000

    deploy: 24e7df1e5c26238dee2ea5352e03856600ecb07a

--- index-628fd6c9445d0d6b.js
+++ index-628fd6c9445d0d6b.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-a5a15728a4b5166a_bg.wasm', import.meta.url);
+        input = new URL('index-628fd6c9445d0d6b_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -517,7 +517,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper251 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper262 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 62, __wbg_adapter_18);
         return addHeapObject(ret);
     };
